0c0229a859a850e7bc0e9edb3cfd9398e4b659b9
chris6801 <chris6801@gmail.com>
2024-09-03 21:26:54 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,60 @@
+import sqlite3
+
+con = sqlite3.connect("inv.db")
+
+cur = con.cursor()
+
+event_stack = []
+
+event_test = ('sale', 2, 4, cur)
+
+event_stack.append(event_test)
+
+def add_entry(table, upc, item, pi):
+    cur.execute(f"""
+        INSERT INTO {table} (upc, item, pi)
+        SELECT ?, ?, ?
+        WHERE NOT EXISTS (SELECT 1 FROM items WHERE upc=?)
+    """, (upc, item, pi, upc))
+
+def modify_entry(table, upc, col, val, cur):
+    cur.execute(f"""
+        UPDATE {table} SET {col} = ? WHERE upc = ? 
+    """, (val, upc))
+
+def parse_event(event_type, upc, amt, cur):
+    if event_type == 'sale':
+        cur.execute("SELECT pi FROM items WHERE upc=?", (upc,))
+        out = cur.fetchone()
+        
+        if out is not None:
+            current_pi = out[0]
+            new_pi = current_pi - amt
+
+            modify_entry('items', upc, 'pi', new_pi, cur)
+        else:
+            print(f"No item found with UPC: {upc}")    
+
+#modify_entry('items', 2, 'pi', 10, cur)
+
+for event in event_stack:
+    e = event_stack.pop()
+    parse_event(*e)
+
+con.commit()
+
+cur.close()
+con.close()
+
+new_con = sqlite3.connect("inv.db")
+new_cur = new_con.cursor()
+
+new_cur.execute("SELECT * FROM items")
+
+res = new_cur.fetchall()
+
+for e in res:
+    print(e)
+
+new_cur.close()
+new_con.close()
